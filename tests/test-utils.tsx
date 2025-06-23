5292ab2f408dc5db0bdfb1e0cd5c992bed4c9118
mariah.freire <mariah.freire@edu.pucrs.br>
2025-06-23 01:27:33 -0300

    test: fix test-utils.tsx imports

--- tests/test-utils.tsx
+++ tests/test-utils.tsx
@@ -2,38 +2,41 @@ import React, { PropsWithChildren } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import userEvent, { UserEvent, Options } from '@testing-library/user-event';
 import { within, screen } from '@testing-library/react';
-import theme from "../src/theme/theme";
-import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material';
+import theme from '../src/theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 export interface CustomRenderOptions extends Omit<RenderOptions, 'queries'> {
-    user?: UserEvent;
-    userProps?: Options;
+  user?: UserEvent;
+  userProps?: Options;
 }
 
-export const customRender = (ui: React.ReactElement, {
+export const customRender = (
+  ui: React.ReactElement,
+  {
     userProps = undefined,
     user = userEvent.setup(userProps),
     ...renderOptions
-}: CustomRenderOptions = {}
+  }: CustomRenderOptions = {}
 ) => {
-    function Wrapper({ children }: PropsWithChildren): JSX.Element {
-        return (      
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                {children}
-            </ThemeProvider>
-        );
-    };
-    return {
-        ...render(ui, {
-            wrapper: Wrapper,
-            ...renderOptions,
-        }),
-        user,
-        within,
-        screen,
-    };
+  function Wrapper({ children }: PropsWithChildren): JSX.Element {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    );
+  }
+
+  return {
+    ...render(ui, {
+      wrapper: Wrapper,
+      ...renderOptions,
+    }),
+    user,
+    within,
+    screen,
+  };
 };
 
 export * from '@testing-library/react';
