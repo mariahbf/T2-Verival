2528d18186f67e954764c418d3ee4e058e52ac72
mariah.freire <mariah.freire@edu.pucrs.br>
2025-06-24 19:24:31 -0300

    fix: fix test report

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,15 +4,23 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    include: ['src/**/*.test.tsx'], 
+    include: ['src/**/*.test.tsx'],
     environment: 'jsdom',
-    globals: true, 
+    globals: true,
     setupFiles: ['./vitest.setup.ts'],
     coverage: {
       provider: 'v8',
       reportsDirectory: './coverage',
-      reporter: ['text', 'html', 'lcov'], 
-      exclude: ['node_modules/', 'tests/', '**/*.test.ts'],
+      reporter: ['text', 'html', 'lcov'],
+      exclude: [
+        'node_modules/',
+        'tests/',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        '.next/',
+        '**/.next/**',
+        '**/vendor-chunks/**',
+      ],
     },
   },
 });
